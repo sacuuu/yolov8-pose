82b3b263b96af87f14825ad36f130db0d0f47250
Sacu <214991731@qq.com>
2024-04-12 20:54:01 +0800

    change train

--- train.py
+++ train.py
@@ -4,9 +4,10 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
 
 # Load a model
-model = YOLO('yolov8n-pose.yaml')  # build a new model from YAML
-model = YOLO('yolov8n-pose.pt')  # load a pretrained model (recommended for training)
-model = YOLO('yolov8n-pose.yaml').load('yolov8n-pose.pt')  # build from YAML and transfer weights
+model = YOLO('yolov8m-pose.yaml')  # build a new model from YAML
+model = YOLO('yolov8m-pose.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8m-pose.yaml').load('yolov8m-pose.pt')  # build from YAML and transfer weights
+
 
 # Train the model
-results = model.train(data='coco8-pose.yaml', workers=0, epochs=100, imgsz=640)
+results = model.train(data='coco-pose.yaml', workers=0, epochs=1, imgsz=640, batch=8)
